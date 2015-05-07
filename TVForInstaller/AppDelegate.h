42fda3f5893f2354747cb71e0829b6f665e8b95c
liamscofield <liamscofield@gmail.com>
2015-05-07 10:01:37 +0800

    Initial Commit

---
+++ TVForInstaller/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TVForInstaller
+//
+//  Created by AlienLi on 15/5/7.
+//  Copyright (c) 2015年 AlienLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
