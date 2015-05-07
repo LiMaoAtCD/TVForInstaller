ac4cba2405ed6dcd91b61127af0fef7343746903
liamscofield <liamscofield@gmail.com>
2015-05-07 10:26:12 +0800

    first commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'TVForInstaller' do
+	pod 'AFNetworking'
+	
+end
+
+target 'TVForInstallerTests' do
+
+end
+
---
+++ TVForInstaller/NetworkingManager.h
@@ -0,0 +1,16 @@
+//
+//  NetworkingManager.h
+//  TVForInstaller
+//
+//  Created by AlienLi on 15/5/7.
+//  Copyright (c) 2015年 AlienLi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AFNetworking.h>
+@interface NetworkingManager : NSObject
+
+
+
+
+@end
