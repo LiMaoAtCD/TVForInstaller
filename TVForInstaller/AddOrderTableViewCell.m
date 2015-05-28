dfa99605741ed88ff6fe5e72b016bfc515633b55
liamscofield <liamscofield@gmail.com>
2015-05-28 10:37:53 +0800

    私单界面

---
+++ TVForInstaller/AddOrderTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  AddOrderTableViewCell.m
+//  TVForInstaller
+//
+//  Created by AlienLi on 15/5/28.
+//  Copyright (c) 2015年 AlienLi. All rights reserved.
+//
+
+#import "AddOrderTableViewCell.h"
+
+@implementation AddOrderTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ TVForInstaller/ZhijiaTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ZhijiaTableViewCell.m
+//  TVForInstaller
+//
+//  Created by AlienLi on 15/5/28.
+//  Copyright (c) 2015年 AlienLi. All rights reserved.
+//
+
+#import "ZhijiaTableViewCell.h"
+
+@implementation ZhijiaTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
