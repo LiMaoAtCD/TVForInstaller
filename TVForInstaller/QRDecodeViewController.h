de8e1f8a8bdee30f7f9f52b57d85b09041ac77f3
liamscofield <liamscofield@gmail.com>
2015-08-19 12:00:32 +0800

    二维码

---
+++ TVForInstaller/QRDecodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  QRDecodeViewController.h
+//  TVForInstaller
+//
+//  Created by AlienLi on 15/8/19.
+//  Copyright (c) 2015年 AlienLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QRDecodeViewController : UIViewController
+
+@end
