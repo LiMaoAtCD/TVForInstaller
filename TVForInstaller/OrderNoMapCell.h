c875ec1c37e47752e78cff4d4e2eb8e1fd08fbb8
liamscofield <liamscofield@gmail.com>
2015-09-21 11:11:24 +0800

    订单界面+ 新建

---
+++ TVForInstaller/OrderNoMapCell.h
@@ -0,0 +1,13 @@
+//
+//  OrderNoMapCell.h
+//  TVForInstaller
+//
+//  Created by AlienLi on 15/9/21.
+//  Copyright © 2015年 AlienLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OrderNoMapCell : UITableViewCell
+
+@end
---
+++ TVForInstaller/OrderNoMapController.h
@@ -0,0 +1,13 @@
+//
+//  OrderNoMapController.h
+//  TVForInstaller
+//
+//  Created by AlienLi on 15/9/21.
+//  Copyright © 2015年 AlienLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OrderNoMapController : UITableViewController
+
+@end
