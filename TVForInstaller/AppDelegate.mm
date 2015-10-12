144474e995b0042c782c9229d5298c28d435bff6
fero2004 <fero2009@126.com>
2015-10-12 14:09:13 +0800

    test

--- TVForInstaller/AppDelegate.mm
+++ TVForInstaller/AppDelegate.mm
@@ -35,7 +35,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    
+    //test
     [self configureTabBarAppearance];
     [self configureBaiduMapSetting];
     [self configureSVProgressHUDAppearance];
